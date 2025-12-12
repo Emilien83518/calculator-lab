9cd4787232d348e1e7eb39fbe37426ecafc00163
Gaspard-f0urc <gaspard.fourcassie@esme.fr>
2025-12-12 16:54:00 +0100

    Add multiply function to calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,7 @@
-def add(n1, n2):
+def add(n1, n2,):
     return n1 + n2
 def f(t):
     return t
+def multiply(n1,n2):
+    return n1*n2
 # TODO: Add more functions below this line (subtract, multiply, divide)
