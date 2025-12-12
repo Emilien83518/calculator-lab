cfaa5e21657aca51daed601b09e54f35a1fd2d42
Etienne-br <etienne.hawk@gmail.com>
2025-12-12 16:54:41 +0100

    Update calculator.py
    ss

--- calculator.py
+++ calculator.py
@@ -2,4 +2,6 @@ def add(n1, n2):
     return n1 + n2
 def f(t):
     return t
+def thomas_est_nul_au_bay_foot(a):
+    return pritn('thomas=loser")
 # TODO: Add more functions below this line (subtract, multiply, divide)
